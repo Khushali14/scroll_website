cc27251642b112594a63e310187b3074ac42f378
khushali <khushalisr2305@gmail.com>
2021-06-08 14:11:56 +0530

    hero section completed

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
+//Display mobile menu
 const mobileMenu = () => {
-    menu.classList.toggle('.is-active');
+    menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active'); 
 }
 
